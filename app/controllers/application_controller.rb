9713a0ae4c29befe5f40fc2bb4458ac50fbc98b9
Euphe <euphetar@gmail.com>
2013-11-04 18:17:04 +0000

    Added shorten method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,9 @@ class ApplicationController < ActionController::Base
   def parsetime(string)
 	string.to_s.slice(11, 5)
   end
-  
+  def shorten(string)
+  string.to_s.slice(0, 3)
+  end
+  helper_method :shorten
   helper_method :parsetime
 end
