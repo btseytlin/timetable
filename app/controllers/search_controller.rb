d930eb1cb055529b3cd43d9bffdcf1098a848f91
Euphe <euphetar@gmail.com>
2013-11-04 18:17:35 +0000

    Added search functionality, ajax search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,25 +1,33 @@
 class SearchController < ApplicationController
 
-	def index
-		@debug = params.to_s
-		@request = params[:search]
-		
-		@results = search(@request)
+  def search
+	
+		@request = params[:search].strip
+    @results = 'none'
+    @results = @request.size > 1 ? get_results(@request) : []
+    
+    respond_to do |format|
+			
+				format.html 
+				format.js 
+			
+		end
 	end
 	
-	def search(string)
+  def get_results(string)
 		results = []
 		Lesson.all.each do |lesson|
-			results.push(lesson) if check_params lesson, string
+			results.push(lesson) if check_params(lesson, string)
 		end
 		
 		return results
 	end
 	def check_params(lesson, request)
-		attrs = [parsetime lesson.time, lesson.teacher, lesson.room, lesson.group.name, lesson.subject, lesson.day]
+    match = false
+		attrs = [parsetime(lesson.time), lesson.teacher, lesson.room, lesson.group.name, lesson.subject, lesson.day]
 		attrs.each do |param|
-			param.to_s.downcase
+      match = true if param.to_s.downcase.include?(request.downcase) 
 		end
-		return true if attrs.include?(request.downcase)
+    return true if match == true
 	end
 end
